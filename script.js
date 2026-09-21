009d66203ccc56c6d891cd8173c8c49e456eb398
RianaRim <cheburashkanataburetke@gmail.com>
2026-09-21 20:22:51 +0300

    initial

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import * as Tone from 'tone'
-
 const synthSettings = {
   volume: 0.8,
   detune: 0,
@@ -30,16 +28,18 @@ function initWebAudio() {
 function initAndStartSynth() {
   const synthNode = new Tone.Synth(synthSettings).toDestination()
 
-  const pattern = new Tone.Pattern(
+  // prettier-ignore
+  const seq = new Tone.Sequence(
     (time, note) => {
-      synthNode.triggerAttackRelease(note, '8n', time)
+      synthNode.triggerAttackRelease(note, 0.1, time)
     },
-    ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4'],
-    'upDown' // "up", "down", "upDown", "random"
-  )
-
-  pattern.interval = '8n'
-  pattern.start(0)
+    [
+      'C4', 'E4', 'G4', 'A4', 'C4', 'E4', 'G4', 'A4',
+      ['C4', 'C4'], ['E4', 'E4'], ['G4', 'G4'], ['A4', 'A4'], ['C4', 'C4'], ['E4', 'E4'], ['G4', 'G4'], ['A4', 'A4'],
+      ['C4', null, null, null], [null, null, 'E4', null], [null, null, 'G4', null], [null, null, null, null],
+      ['C4', 'C4', 'C4', 'C4'], ['E4', 'E4', 'E4', 'E4'], ['G4', 'G4', 'G4', 'G4'], ['A4', 'A4', 'A4', 'A4']
+    ]
+  ).start(0)
 
   const transport = Tone.getTransport()
   transport.bpm.value = 60
